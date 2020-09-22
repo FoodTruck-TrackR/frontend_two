557ea409caa03f8cb6996fd84eea002938db0cd8
Joe-Panetta <jlpanetta1681@icloud.com>
2020-09-22 12:57:12 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+// import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import  Form  from  "./components/pages/Form"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const defaultValues = {
+  name: "",
+  email:"",
+
+};
+const defaultErrors = {
+  name: "",
+  email: "",
+  
 }
 
-export default App;
+
+function App() {
+  //form values
+  const [formValues, setFormValues] = useState(defaultValues);
+  // store the values from the form
+  const [savedFormInfo, setSavedFormInfo] = useState([]);
+  const [errors, setErrors] = useState(defaultErrors);
+
+  const change = (evt) => {
+    const { name, value } = evt.target;
+    setFormValues({...formValues, [name]: value });
+  };
+  const submit = (evt) => {
+    evt.preventDefault();
+    
+    const newData = {
+      name: formValues.name.trim(),
+      email: formValues.email.trimend()
+    };
+    setSavedFormInfo([...savedFormInfo, newData]);
+    setFormValues(defaultValues);
+  };
+  
+      return (
+        <div className="App">
+        <Form formValues={formValues} change={change} submit={submit}/>
+   
+         
+         
+        
+           <Router>
+           <Route>
+             <Navbar />
+             
+             <Switch>
+               <Route path='./components/Form' exact component={Form} />
+               <Route path='/services' component={Services} />
+               <Route path='/products' component={Products} />
+               <Route path='/sign-up' component={SignUp} />
+             </Switch>
+          </Route> 
+ 
+            </Router>
+      </div> 
+); 
+}
+
+  
+     
+     
+     export default App;
